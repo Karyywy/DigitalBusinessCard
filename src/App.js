4a02d740ba5d4165792b3b7572f857b4b74ff670
Kary Young <128840281+Karyywy@users.noreply.github.com>
2023-12-12 23:13:55 +0800

    update src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";  
+import Profile from "./components/Profile"; 
+import Info from "./components/Info";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(){
+  return(
+    <div className="card">
+      <Profile />
+      <Info />
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
